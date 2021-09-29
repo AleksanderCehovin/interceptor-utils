8596840b95e3e18af9845052c6e4915a3acf28de
Aleksander <aleksander.cehovin@maxiv.lu.se>
2021-09-29 14:40:14 +0200

    Updated scripts for faster and realistic FPS rates. Modified to support latest Interceptor GUI and the dozor modification of the Interceptor GUI

--- src/task_GUI.py
+++ src/task_GUI.py
@@ -11,8 +11,20 @@ import sys
 import time
 import zmq
 
-
+#Connection port to feeding process
 PORT=5557
+
+#Sleep time between collection attempts
+SLEEP_S = 0
+
+#Print all frames?
+PRINT_ALL = False
+PRINT_DELIM = "***************************************************"
+
+#Print Interval in frames
+PRINT_INT_FRAMES = 500
+
+
 
 context = zmq.Context()
 
@@ -21,14 +33,28 @@ receiver = context.socket(zmq.PULL)
 receiver.connect("tcp://localhost:{}".format(PORT))
 receiver.bind("tcp://*:{}".format(PORT))
 
-
+fps_counter = 0
+fps_time_start = time.time()
 # Process tasks forever
 while True:
     s = receiver.recv_string()
 
-    sys.stdout.write("Rx: " + s + "\n")
-    sys.stdout.flush()
+    if PRINT_ALL:
+        #For high frame rates it's a bad idea
+        #sys.stdout.write("Rx: " + s + "\n")
+        #sys.stdout.flush()
+        pass
+
+    fps_counter += 1
+    if fps_counter % PRINT_INT_FRAMES == 0:
+        deltaT = time.time() - fps_time_start
+        sys.stdout.write("FPS:{:.2f}\n".format(fps_counter/deltaT))
+        if not PRINT_ALL:
+            sys.stdout.write("Rx: " + s + "\n" + PRINT_DELIM + "\n")
+        sys.stdout.flush()
+        fps_time_start = time.time()
+        fps_counter = 0
 
     # Do the work
-    time.sleep(0.01)
+    time.sleep(SLEEP_S)
 
--- src/task_data_generator.py
+++ src/task_data_generator.py
@@ -15,23 +15,37 @@ import random
 #Total number of data points. I think this data
 #allocated memory on the GUI side, don't go astronomical
 #with this variable.
-N=20000
+N=150000
 
 #Number of data points per run
-RUN_LENGTH=2500
+RUN_LENGTH=15000
 
 #Maximum number of runs. Each run generates a tab in the GUI, which
 #allocates memory. Don't set this to astronomical values.
 MAX_NO_RUNS=10
 
 #Rate of indexed frames as a modulo number 
-INDEXED_RATE=10
+INDEXED_RATE=20
 
 #Port number for stream socket
 PORT=5557
 
+# Sleep time between frames. Main parameter for effective FPS.
+SLEEP_S = 0.005
+
+# STD OUT PRINT INTERVAL
+PRINT_INT_FRAMES = 500
+
 def get_spot_number(index,period):
     return int(10000*abs(math.sin(index/period)))
+
+def get_res(index,period):
+    value = math.sin(index/period)
+    return 5*abs(value*value*value)
+
+def get_quality(index,period):
+    value = math.cos(index/period)
+    return 100*abs(value*value)
 
 try:
     raw_input
@@ -57,6 +71,8 @@ print("Sending data to GUI...")
 #Send a data string N times to GUI
 run_no=0
 img_no=0
+fps_counter=1
+fps_time_start = time.time()
 for i in range(0,N):
     #Set up a new run which generates a new tab in GUI
     if img_no % RUN_LENGTH == 0 and run_no <= MAX_NO_RUNS:
@@ -67,13 +83,21 @@ for i in range(0,N):
     indexed="NA"
     if img_no % INDEXED_RATE == 0:
         indexed="True"
-    #Generate some kind of spot number variation
+    #Generate data for no. spots, resolution, and quality for every frame
     no_spots=get_spot_number(img_no,period)
-    #Define message string to be sent to GUI
-    data=u"run {} frame {} result  {} {} {} {} {} {} {} {}  mapping {}".format(run_no,img_no,no_spots,4,5,1.33,7,8,indexed,10,11)
+    hres = get_res(img_no,period)
+    quality = get_quality(img_no,period)
+    #Define message string to be sent to GUI. This is the Interceptor GUI format.
+    data=u"run {} frame {} result  {} {} {} {} {} {} {} {}  mapping {}".format(run_no,img_no,no_spots,4,quality,hres,7,8,indexed,10,11)
     sender.send_string(data)
     img_no+=1    
-    time.sleep(0.01)
-
+    #Handle FPS count
+    fps_counter += 1
+    if fps_counter % PRINT_INT_FRAMES == 0:
+        deltaT = time.time() - fps_time_start
+        print("FPS:{:.2f}".format(fps_counter/deltaT))
+        fps_time_start = time.time()
+        fps_counter = 0
+    time.sleep(SLEEP_S)
 # Give 0MQ time to deliver
 time.sleep(1)
